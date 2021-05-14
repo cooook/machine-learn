70350ea9b2820906c719f07462fcf72125e4cd42
张一帆 <cooook@Cooooks-MacBook-Pro.local>
2021-05-14 15:15:16 +0800

    update 5-zhe

--- ch8/BoostMain.py
+++ ch8/BoostMain.py
@@ -1,6 +1,7 @@
 from sklearn import tree, metrics
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 def AdaBoost(dataset, label, test_set, test_label, T, cnt):
@@ -9,6 +10,7 @@ def AdaBoost(dataset, label, test_set, test_label, T, cnt):
     test_size = len(test_set)
     w = np.ones(train_size) / train_size
     Result = np.zeros(test_size)
+    AUC = []
     for i in range(T):
         clf.fit(dataset, label, sample_weight=w)
         train_pred = np.array(clf.predict(dataset))
@@ -21,11 +23,11 @@ def AdaBoost(dataset, label, test_set, test_label, T, cnt):
         alpha = 0.5 * np.log((1 - error_rate) / error_rate)
         Result = Result + alpha * test_pred
         if i % cnt == 0:
-            Times.append(i)
             FinalResult = np.sign(Result)
             AUC.append(metrics.roc_auc_score(test_label, FinalResult))
         Z = 2 * np.sqrt(error_rate * (1 - error_rate))
         w = w * ([np.exp(-alpha * x) for x in error_two]) / Z
+    return AUC
 
 def fromZerotoOne(x):
     if x == 1:
@@ -33,20 +35,33 @@ def fromZerotoOne(x):
     return -1
 
 if __name__ == '__main__':
-    dataset = np.loadtxt("./ch8/adult_train_feature.txt")
-    label = np.loadtxt("./ch8/adult_train_label.txt")
-    test_set = np.loadtxt("./ch8/adult_test_feature.txt")
-    test_label = np.loadtxt("./ch8/adult_test_label.txt")
-    Times = []
-    AUC = []
+    dataset = np.loadtxt("./ch8/adult_dataset/adult_train_feature.txt")
+    label = np.loadtxt("./ch8/adult_dataset/adult_train_label.txt")
+    test_set = np.loadtxt("./ch8/adult_dataset/adult_test_feature.txt")
+    test_label = np.loadtxt("./ch8/adult_dataset/adult_test_label.txt")
+
+    best_T = 4
+    Total_T = 100
+    print_cnt = 10
+    test_cnt = 5
+    best_AUC = 0
 
     label = np.array(list(map(fromZerotoOne, label)))
     test_label = np.array(list(map(fromZerotoOne, test_label)))
 
-    AdaBoost(dataset, label, test_set, test_label, 500, 10)
+    # AdaBoost(dataset, label, test_set, test_label, 500, 10)
+    X = [x for x in range(0, Total_T, print_cnt)]
+    y = np.zeros(len(X))
+
+
+    for i in range(test_cnt):
+        train_X, test_X, train_y, test_y = train_test_split(dataset, label, test_size=0.2)
+        tmp = AdaBoost(train_X, train_y, test_X, test_y, Total_T, print_cnt)
+        y += tmp
+    y = y / test_cnt
 
     plt.figure(figsize=(6, 6))
-    plt.plot(Times, AUC, color="red", linewidth=1)
+    plt.plot(X, y, color="red", linewidth=1)
     plt.xlabel("Num")  # xlabel、ylabel：分别设置X、Y轴的标题文字。
     plt.ylabel("AUC")
     plt.ylim(0.6, 0.85)  # xlim、ylim：分别设置X、Y轴的显示范围。
