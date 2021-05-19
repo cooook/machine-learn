2360129c589c054a85f652c2cd0851e301d4723e
张一帆 <cooook@Cooooks-MacBook-Pro.local>
2021-05-19 14:36:27 +0800

    2.5/4

--- xxaqzps/match.py
+++ xxaqzps/match.py
@@ -67,8 +67,14 @@ def find_value_in_father(node):
 新建一个send_node节点, 父亲为ast上first_node的父亲
 要修改node 即 call的所有children 的父亲和first_node 的孩子， 注意孩子的顺序. 
 '''
+
+
 def change_calldotvalue(first_node, node):
-    tmp_attributes = json.loads('{"attributes":{"argumentTypes":[{"typeIdentifier":"t_uint256","typeString":"uint256"}],"isConstant":false,"isLValue":false,"isPure":false,"lValueRequested":false,"member_name":"send","referencedDeclaration":null,"type":"function (uint256) returns (bool)"}}')['attributes']
+    tmp_attributes = json.loads(
+        '{"attributes":{"argumentTypes":[{"typeIdentifier":"t_uint256","typeString":"uint256"}],"isConstant":false,'
+        '"isLValue":false,"isPure":false,"lValueRequested":false,"member_name":"send","referencedDeclaration":null,'
+        '"type":"function (uint256) returns (bool)"}}')[
+        'attributes']
     send_node = Node(nodeType='function (uint256) returns (bool)', father=first_node.father)
     send_node.parseAttributes(tmp_attributes)
     # 改儿子的父亲
@@ -85,7 +91,6 @@ def change_calldotvalue(first_node, node):
     return send_node
 
 
-
 def MatchReentrancy(node):
     if node.member_name == 'call':
         first_node = find_value_in_father(node)
@@ -95,6 +100,7 @@ def MatchReentrancy(node):
     for ch in node.children:
         MatchReentrancy(ch)
 
+
 def find_judge_in_father(node):
     while node:
         if node.name == 'IfStatement':
@@ -103,13 +109,14 @@ def find_judge_in_father(node):
 
     return False
 
+
 def CheckReturnValue(node):
     UnaryOperation_node = Node()
-    UnaryOperation_node.parseAttributes(json.loads('{"argumentTypes":null,"isConstant":false,"isLValue":false,"isPure":false,"lValueRequested":false,"operator":"!","prefix":true,"type":"bool"}'))
+    UnaryOperation_node.parseAttributes(json.loads(
+        '{"argumentTypes":null,"isConstant":false,"isLValue":false,"isPure":false,"lValueRequested":false,"operator":"!","prefix":true,"type":"bool"}'))
     UnaryOperation_node.name = "UnaryOperation"
     father_node = node.father
     UnaryOperation_node.children.append(father_node)
-
 
     IfStatement_node = Node()
     IfStatement_node.parseAttributes(json.loads('{"falseBody":null}'))
@@ -117,11 +124,12 @@ def CheckReturnValue(node):
     IfStatement_node.children.append(UnaryOperation_node)
     UnaryOperation_node.father = IfStatement_node
 
-    Throw_node = Node()
-    parseAst(json.loads('{"children":[{"children":[],"id":25,"name":"Throw","src":"233:5:0"}],"id":26,"name":"Block"}'), IfStatement_node)
+    parseAst(json.loads('{"children":[{"children":[],"id":25,"name":"Throw","src":"233:5:0"}],"id":26,"name":"Block"}'),
+             IfStatement_node)
 
+    # throw 可能存在问题， 这个版本的throw 直接向IfStatement_node增加一个节点
 
-    pass 
+    pass
 
 
 def MatchUncheckReturnValue(node):  # 对低级函数调用进行匹配
@@ -130,3 +138,65 @@ def MatchUncheckReturnValue(node):  # 对低级函数调用进行匹配
             CheckReturnValue(node)
     for ch in node.children:
         MatchUncheckReturnValue(ch)
+
+'''
+对于 二目运算 a (x)= b
+使用SafeMath库中的函数来替代
+SafeMath's node -> ls = 之前表达式的node
+rs = 运算表达式node
+对于 a x b
+直接转换， 不需要改变其他的东西
+'''
+def MatchOperator(opt):
+    if opt == '+=':
+        return ['add', 1]
+    if opt == '-=':
+        return ['sub', 1]
+    if opt == '*=':
+        return ['mul', 1]
+    if opt == '+':
+        return ['add', 0]
+    if opt == '-':
+        return ['sub', 0]
+    if opt == '*':
+        return ['mul', 0]
+    return None
+
+def UsingSafeMathLibrary(node, mode):
+    # mode[1] 为 1 代表是 'x=' 需要增加等号 和 左值
+    if mode[1]:
+        pass
+    else:
+        FirstNode = Node()
+        SubNode = Node()
+        SafeMathNode = Node()
+        FirstNode.parseAttributes('{"attributes":{"argumentTypes":null,"isConstant":false,"isLValue":false,"isPure":false,"isStructConstructorCall":false,"lValueRequested":false,"names":[null],"type":"uint256","type_conversion":false}}')
+        SubNode.parseAttributes('{"attributes":{"argumentTypes":[{"typeIdentifier":"t_uint256","typeString":"uint256"},{"typeIdentifier":"t_uint256","typeString":"uint256"}],"isConstant":false,"isLValue":false,"isPure":false,"lValueRequested":false,"member_name":"sub","referencedDeclaration":68,"type":"function (uint256,uint256) pure returns (uint256)"}}')
+        SafeMathNode.parseAttributes('{"attributes":{"argumentTypes":null,"overloadedDeclarations":[null],"referencedDeclaration":93,"type":"type(library SafeMath)","value":"SafeMath"}}')
+        FirstNode.name = 'FunctionCall'
+        SubNode.name = 'MemberAccess'
+        SafeMathNode.name = 'Identifier'
+        SafeMathNode.father = SubNode
+        SubNode.children.append(SafeMathNode)
+        for x in range(len(node.father)):
+            if node.father.children[x] == node:
+                node.father.children[x] = FirstNode
+        FirstNode.father = node.father
+        FirstNode.children.append(SubNode)
+        for x in node.children:
+            FirstNode.children.append(x)
+            x.father = FirstNode
+
+    # node1 -> children = [SubNode[SafeMathNode], balances[sender[msg]], _amount]
+    # SubNode -> father.children = node1
+
+
+def MatchUnsafeMath(node):
+    if 'operator' in node.attributes:
+        opt = node.attributes['operator']
+        mode = MatchOperator(opt)
+        if mode:
+            UsingSafeMathLibrary(node, mode)
+
+if __name__ == '__main__':
+    pass
