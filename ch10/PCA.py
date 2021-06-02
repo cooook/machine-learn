ee3df1c80b8c147cfdd6e4531fe6f27765f6808d
Cooook <zyfan020305@gmail.com>
2021-06-02 22:46:41 +0800

    Update

--- ch10/PCA.py
+++ ch10/PCA.py
@@ -7,16 +7,31 @@ def loadImage(path):
     img = Image.open(path)
     img = img.convert("L")
     data = img.getdata()
-    data = np.array(data).reshape(1, -1)/100
+    data = np.array(data).reshape(1, -1)[0]
     return data
 
 
 
-path = '/Users/cooook/Desktop/machine learn/ch10/yalefaces/'
+path = 'D:\\zyf\\machine-learn\\ch10\\yalefaces\\'
 
 files = os.listdir(path)
 
 img = []
 pca = PCA(n_components=20)
-
-
+for file in files:
+    if file != '.DS_Store':
+        img.append(loadImage(path + file))
+img = np.array(img)
+newImg = pca.fit_transform(img)
+Matrix = pca.components_
+# newImg = pca.transform(img)
+for i in range(len(newImg)):
+    if files[i] == '.DS_Store':
+        continue
+    image = newImg[i]
+    tmp = np.dot(image, Matrix)
+    tmp = tmp.reshape(243, 320)
+    tmp += np.mean(img, axis=0).reshape(243, 320)
+    new_img = Image.fromarray(tmp.astype('uint8'))
+    new_img = new_img.convert("L")
+    new_img.save('D:\\zyf\\machine-learn\\ch10\\mydata\\{0}.png'.format(files[i]))
